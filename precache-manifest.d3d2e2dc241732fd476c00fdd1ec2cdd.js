c866b6a2bc3a449c975c1ba03b490d3a9fa05dfa
wtkim <rladnjsxo123@naver.com>
2019-05-27 18:47:47 +0900

    Updates

---
+++ precache-manifest.d3d2e2dc241732fd476c00fdd1ec2cdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da924cef294bf18fe329f2263a548644",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "97ff7ca534dc9ad47475",
+    "url": "/movie_app/static/css/main.8bf46b57.chunk.css"
+  },
+  {
+    "revision": "5cdb4062c6dfdd077298",
+    "url": "/movie_app/static/js/2.31ebf22a.chunk.js"
+  },
+  {
+    "revision": "97ff7ca534dc9ad47475",
+    "url": "/movie_app/static/js/main.eb70920f.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
